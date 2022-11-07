d03721141230f450ffb8d82c3e67b95802ad8760
gyl <172325760@qq.com>
2022-11-07 14:34:56 +0800

    提交

--- make.js
+++ make.js
@@ -1,18 +1,1 @@
-const path = require('path');
-const fs = require('fs');
-
-let componentSrc;
-let SRC = 'component';
-let com = {};
-let BOOTJS = path.join(__dirname,SRC,'boot');
-let bootSrc = fs.readFileSync(BOOTJS,'utf8');
-fs.readdirSync(SRC).forEach(function(file){
-  let _file = path.join(SRC,file);
-  if(fs.lstatSync(_file).isDirectory()){
-    let p = com;
-    p[file]=path.join(__dirname,SRC,file)
-  }
-});
-componentSrc = JSON.stringify(com,null,2);
-let bootSrcJS = bootSrc.replace(/component/,componentSrc);
-fs.writeFileSync(BOOTJS+'.js',bootSrcJS,'utf8')
+console.log(1111111111111111)
