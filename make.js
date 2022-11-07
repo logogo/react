5b78ec28566641d08e2d318a554bbc8a3165111c
gyl <172325760@qq.com>
2022-11-07 14:42:38 +0800

    提交

--- make.js
+++ make.js
@@ -4,10 +4,8 @@ let diff = getDiffFiles()
 
 function getDiffFiles(type){
   let root = process.cwd()
-  let files = execSync("git diff --cached --name-status HEAD")
-  .toString()
-  .split("\n")
-console.log('git diff files:', files)
+  let files = execSync("git diff --cached --name-status HEAD").toString().split("\n")
+console.log('git diff files:', execSync("git diff --cached --name-status HEAD"))
 console.log(root)
   return 1111111
 }
