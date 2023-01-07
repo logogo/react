8b07969e312d8c8bb5c4ac2ee6640f8da82735aa
gyl <172325760@qq.com>
2023-01-07 19:23:26 +0800

    提交

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
-import appData from './App/reducer/reducer.js'
+import appData from './App/reducer/reducer.js';
 import AntdLiData from './AntdLi/reducer/reducer.js';
 const finalReducer = combineReducers({
     appData,
     AntdLiData
-})
+});
 
 export default finalReducer;
