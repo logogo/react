61b347d2190a284bce0c1310a30fb5c6f29fc5bc
gyl <172325760@qq.com>
2023-01-08 10:39:32 +0800

    提交

--- src/component/container.js
+++ src/component/container.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import oStyle from './container.css'
+import oStyle from './container.css';
 
-export default class Container extends Component{
-    constructor(props){
-        super(props)
-    }
-    render(){
-        return (<div className={oStyle.red}>{this.props.children}</div>)
+export default class Container extends Component {
+    render() {
+        return (<div className={oStyle.red}>{this.props.children}</div>);
     }
 }
