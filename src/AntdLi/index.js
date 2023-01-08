2ffe61a18f00b95f4ce6b7867431f89c932c2d4c
gyl <172325760@qq.com>
2023-01-08 09:53:57 +0800

    提交

--- src/AntdLi/index.js
+++ src/AntdLi/index.js
@@ -1,18 +1,18 @@
-import React, { useEffect,useState,useMemo }  from 'react';
-import { useSelector,useDispatch } from 'react-redux'
+import React, { useEffect, useState, useMemo } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Row, Col } from 'antd';
 import ShowNum from './compoent/showNum';
 
 const AntdLi = () => {
-  const [num, setNum] = useState(0)
-  const dispatch = useDispatch()
-  const counter = useSelector(state => state. AntdLiData)
+  const [num, setNum] = useState(0);
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state. AntdLiData);
   useEffect(()=>{
     dispatch({
       type: "acts",
       num: 222
     })
-  },[])
+  },[]);
   const add = ()=>{
     setNum(num+1)
   }
