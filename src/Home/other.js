31b6a36120261bf757b27daff8fbb97ed22b1a96
gyl <172325760@qq.com>
2023-01-08 10:52:20 +0800

    提交

--- src/Home/other.js
+++ src/Home/other.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 
 class Other extends Component {
-    static open(){
-        alert('a')
+    static open() {
+        alert('a');
     }
-    render(){
-        return null
+    render() {
+        return null;
     }
 }
 
