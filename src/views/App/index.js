66c47afc67ddb9b7d089949561c3f6fe42cf32cc
gyl <172325760@qq.com>
2023-03-11 16:16:16 +0800

    提交

--- src/views/App/index.js
+++ src/views/App/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import oStyle from './index.less';
-import Menu from './component/menu/index.js';
+import Menu from './component/menu/index';
 
-console.log(oStyle);
-console.log(11111);
 const app = () => {
     return (
         <div className={oStyle.container}>
             <Menu/>
-            <div>22222222</div>
         </div>
     );
 };
