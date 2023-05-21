511a8eb2f8b3248818ffbaf79486e5239df6d141
gyl <172325760@qq.com>
2023-05-21 23:55:35 +0800

    feat: 提交

--- src/router/permission.js
+++ src/router/permission.js
@@ -1,16 +1,13 @@
-import React from "react"
-import { useRouteLoaderData } from 'react-router-dom'
+import React from 'react';
+import { useRouteLoaderData } from 'react-router-dom';
 
-
-const Permission = (props) => {
-   // 这个root是我们在前面路由中定义了 id: 'root'
-   const { children, code } = props;
-   // const loaderData = useRouteLoaderData(code);
-   // if(!code || loaderData?.permissionRoutes?.includes(code)) {
-   //    return <>{children}</>
-   // }
-   return <>{children}</>
-   // return <div>403...</div>
-}
+const Permission = props => {
+    // 这个root是我们在前面路由中定义了 id: 'root'
+    const { children, code } = props;
+    const loaderData = useRouteLoaderData(code);
+    if (!code || loaderData?.permissionRoutes?.includes(code)) {
+        return <>{children}</>;
+    }
+};
 
 export default Permission;
