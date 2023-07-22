6a7a75cb576cbc98f58e6072eb09baa39941f41b
gyl <172325760@qq.com>
2023-07-22 15:56:44 +0800

    feat: 提交

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ChildCom from './childCom';
 interface ceType {
     name: string;
     age: number;
 }
 const App = () => {
+    const [count, setCount] = useState<number>(0);
     const ce: ceType = {
         name: '1111',
         age: 12
     };
+    const change = (e: React.MouseEvent) => {
+        console.log(e);
+        setCount(100);
+    };
     return (
         <>
-            <div>111111</div>
-            <ChildCom age={ce.age} />
+            <div onClick={e => change(e)}>111111</div>
+            <div>{count}</div>
+            <ChildCom age={ce.age} count={111}>
+                <div>666666</div>
+            </ChildCom>
         </>
     );
 };
--- src/childCom.tsx
+++ src/childCom.tsx
@@ -1,13 +1,21 @@
-import React, { FC } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 interface childType {
     age: number;
+    count: number;
+    children: React.ReactNode;
 }
 
-const ChildCom: FC<childType> = ({ age }) => {
+const ChildCom: React.FC<childType> = ({ age, count, children }) => {
+    const node = useRef(null);
+    useEffect(() => {
+        console.log(node);
+    }, []);
     return (
         <>
-            <div>{age}</div>
+            <div ref={node}>{age}</div>
+            {children}
+            <div>{count}</div>
         </>
     );
 };
